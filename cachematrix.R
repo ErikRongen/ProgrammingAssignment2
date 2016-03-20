4df7689ace5007b26cb93f990782486817d29204
ErikRongen <e.rongen@bi-architects.nl>
2016-03-20 19:19:21 +0100

    Completed ProgrammingAssignment2
    Two functions implemented to calculate the Invers of a Matrix and caching the result.
    Any consequent calls for the result will be taken from cache rather than recalculated.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculation of the Inverse of a Matrix,
+## but returning a cached result if the Inverse of this Matrix has already been calculated earlier
 
-## Write a short comment describing this function
+## Usage
+## y <- makeCacheMatrix(x = matrix())
+## z1 <- cacheSolve(y)
+## z2 <- cacheSolve(y)
+
+## makeCacheMatrix(x = matrix())
+## Construct the cached version of matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(y)
+## y is a matrix constructed by the function makeCacheMatrix
+## the result is either calculated or returned from cache if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
